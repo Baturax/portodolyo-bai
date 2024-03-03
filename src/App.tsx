da173af81892e8b893b79b2772e4698989ace8c6
baturax <batuhanakkurt000@gmail.com>
2024-03-03 03:38:46 +0300

    fddf

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 
         <a href='https://github.com/baturax' className='tabboslugu'><h2>github hesabım</h2></a>
         <a href='https://instagram.com/batuhaaaaaaaaaaaaaaaan' className='tabboslugu'><h2>insta hesabım</h2></a>
-        <a href='' className='tabboslugu'><h2>git reposu</h2></a>
+        <a href='https://github.com/Baturax/portodolyo-bai' className='tabboslugu'><h2>git reposu</h2></a>
 
       </header>
 
@@ -20,13 +20,13 @@ function App() {
         <img src={require('./img/ana.png')}
         className='portofolyoresim'/>
 
-        <h2>öyle kafama göre takılıyorum </h2>
+        <h2>öyle kafama göre takılıyorum \N arch linux kullaniom</h2>
 
         </header>
 
         <header className="yaziyeri">
         <h3 className='ilkmetin'>ben batuhan</h3>
-        <h3 className='ilkmetin'>malım gerizekalıyım müzik dinlerim salağım</h3>
+        <h3 className='ilkmetin'>müzik dinlerim</h3>
         <h3 className='ilkmetin'>pazardansonra pazartesiyi severim</h3>
 
       </header>
