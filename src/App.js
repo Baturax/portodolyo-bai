f79c5735fb7d4e205828004c69fea10ef7b513a9
baturax <batuhanakkurt000@gmail.com>
2024-04-06 11:10:36 +0300

    reer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+
+  const [data, setdata] = useState([]);
+
+   useEffect(() => {
+      fetch('https://api.lanyard.rest/v1/users/1224088617270382763')
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            setdata(data);
+         })
+
+   }, []);
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1></h1>
+
       </header>
     </div>
   );
